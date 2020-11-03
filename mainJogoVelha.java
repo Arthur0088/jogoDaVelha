c27352a4431316f76cf2eafcda658eb27db6dde7
Arthur0088 <63612886+Arthur0088@users.noreply.github.com>
2020-11-02 21:33:06 -0300

    Add files via upload

---
+++ mainJogoVelha.java
@@ -0,0 +1,17 @@
+package jogo;
+
+public class mainJogoVelha {
+
+	public static void main(String[] args) {
+		
+		jogoVelha game=new jogoVelha();
+		
+		
+		
+		game.MostrarMatriz();
+		game.Ganhador();
+	
+
+	}
+
+}
